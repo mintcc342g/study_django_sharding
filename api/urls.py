19b07466cecd3dd576393428d4b5d1cb559d5061
mintcc342g <mintcc342g@gmail.com>
2020-08-20 18:54:10 +0900

    add urls

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,6 @@ from .views import *
 
 urlpatterns = [
     path("v1/user", UserViewSet.as_view({"get": "list", "post": "create"}), name="users"),
-    path("v1/user/<int:user_id>/post", PostViewSet.as_view({"get": "list", "post": "create"}), name="posts"),
-    path("v1/user/<int:user_id>/post/<int:post_id>", PostViewSet.as_view({"get": "retrieve", "put": "update"}), name="post"),
+    path("v1/user/<int:user_id>/post", PostViewSet.as_view({"get": "list", "post": "add"}), name="posts"),
+    # path("v1/user/<int:user_id>/post/<int:post_id>", PostViewSet.as_view({"get": "retrieve", "put": "update"}), name="post"),
 ]
